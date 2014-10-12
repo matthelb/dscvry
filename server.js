08f5dfb94359f5f22a5eaf1b6ee9e7cc980b4d8b
Matthew Burke <matthelb@usc.edu>
2014-10-11 21:10:00 -0700

    Move main file to base directory and rename server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-#!/usr/bin/env node
 var debug = require('debug')('dscvry');
-var app = require('../app');
+var app = require('./app');
 
-app.set('port', process.env.PORT || 8888);
+app.set('port', 80);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
